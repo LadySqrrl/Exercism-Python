1f96af38fa6ef4bc716dc33cf888c7a6784be4b7
LadySqrrl <72673087+LadySqrrl@users.noreply.github.com>
2021-04-24 16:11:53 -0500

    fixed two-fer

--- two-fer/two_fer.py
+++ two-fer/two_fer.py
@@ -1,6 +1,2 @@
-def two_fer(name=None):
-    if name == None:
-        return 'One for you, one for me.'
-    else:
-        return 'One for ' + name + ', one for me.'
-    pass
+def two_fer(name='you'):
+        return f'One for {name}, one for me.'
